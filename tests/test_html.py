f1dd3e8c2e09f511840c1cfa8243fb83a6174705
Javier Sancho <jsf@jsancho.org>
2020-09-18 10:31:23 +0200

    Test HTTP redirection

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,16 +1,16 @@
-from .fixtures import app_client
+from .fixtures import app_client, app_client_with_hash
 
 def test_homepage(app_client):
-    response = app_client.get('/', gather_request=False)
+    response = app_client.get('/')
     assert response.status == 200
     assert 'dummy_tables' in response.text
 
-def test_database_page(app_client):
-    response = app_client.get('/dummy_tables', allow_redirects=False, gather_request=False)
+def test_database_page(app_client_with_hash):
+    response = app_client_with_hash.get('/dummy_tables', allow_redirects=False)
     assert response.status == 302
-    response = app_client.get('/dummy_tables', gather_request=False)
+    response = app_client_with_hash.get('/dummy_tables')
     assert 'dummy_tables' in response.text
 
 def test_table(app_client):
-    response = app_client.get('/dummy_tables/table2', gather_request=False)
+    response = app_client.get('/dummy_tables/table2')
     assert response.status == 200
